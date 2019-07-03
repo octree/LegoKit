8ee2290a1e9402564f24ede28bc8f313e0b84888
Octree <fouljz@gmail.com>
2019-07-03 20:53:07 +0800

    🔥 Removing test codes

--- Tests/LegoKitTests/LegoKitTests.swift
+++ Tests/LegoKitTests/LegoKitTests.swift
@@ -3,10 +3,6 @@ import XCTest
 
 final class LegoKitTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(LegoKit().text, "Hello, World!")
     }
 
     static var allTests = [
